694e66bdacb1f8a13c5d7f6fb1bef0fe46da1643
kawasaki <k.kawasaki0820@gmail.com>
2019-12-09 21:49:40 +0900

    課題2の修正1

--- script.js
+++ script.js
@@ -2,19 +2,25 @@
 
 let idCnt = 0;
 
+const tdAdd = (content, tr) => {
+    const td = document.createElement('td');
+    td.innerHTML = content;
+    tr.appendChild(td);
+    return tr;
+};
+
 document.getElementById('addBtn').addEventListener('click',(event)=>{
     event.preventDefault();
     const id = idCnt;
     const comment = document.getElementById('form').comment.value;
 
-    const addContent = 
-    `<tr>
-        <td>${idCnt}</td>
-        <td>${comment}</td>
-        <td><button>作業中</button></td>
-        <td><button>削除</button></td>
-    </tr>`;
-    document.getElementById('data').insertAdjacentHTML('beforeend',addContent);
+    let tr = document.createElement('tr');
+    tr = tdAdd(id,tr);
+    tr = tdAdd(comment,tr);
+    tr = tdAdd('<button>作業中</button>',tr);
+    tr = tdAdd('<button>削除</button>',tr);
+    document.getElementById('addData').appendChild(tr);
+
     document.getElementById('form').comment.value = "";
     idCnt++;
 });
